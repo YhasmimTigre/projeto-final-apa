c43c734e101c07c75537c018b46695142c3373be
Yhasmim <93266317+YhasmimTigre@users.noreply.github.com>
2025-04-13 18:27:02 +0000

    add ctime

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <ctime> 
 #include "src/algorithms/alocador.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
+    //realiza a leitura do arquivo
     if (argc != 2) {
         std::cerr << "Uso: " << argv[0] << " <arquivo_entrada>\n";
         return 1;
     }
 
+    //executa o guloso e calcula tempo médio de execução
+    clock_t start = clock();
     if (!executarAlocacao(argv[1])) {
         return 1;
     }
+    clock_t end = clock();
+    double elapseG = end - start;
+    //TempoMedioExecucao(elapseG);
+
+    //executa o VND e calcula tempo médio de execução
+    clock_t start = clock();
+    VND();
+    clock_t end = clock();
+    double elapseVND = end - start;
+    //TempoMedioExecucao(elapseVND);
+
+    //funcao TabelaFinalResultados(); // solução final, tempo medio, gap
+
     return 0;
 }
